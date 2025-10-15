afa2f3700c158f724732e50837565721e4d92ba8
Rishi <rishi@Rishis-MacBook-Air.local>
2025-10-15 22:16:47 +0530

    changed

--- donationinventoryfrontend/src/config.js
+++ donationinventoryfrontend/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  url: "http://localhost:2030/donationbackend/api/donations"
+  // Base URL where your backend is deployed
+  API_BASE_URL: "http://localhost:2030/donationbackend/api/donations",
 };
 
 export default config;
